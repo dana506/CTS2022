cddd245b719cb7039a60504de0b8eb066dd47694
Dana <buzatudana15@stud.ase.ro>
2022-04-04 20:14:53 +0300

    Don't Repeat Yourself (Avoid Duplication)

--- Assignment 2/src/assignment/Account.java
+++ Assignment 2/src/assignment/Account.java
@@ -1,17 +1,25 @@
 package assignment;
 
+
 public class Account {
+
+	public static final float BROKER_FEE = .0125f;
+	public static final float DAYS_IN_A_YEAR = 365;
 	public double loanValue, monthlyRate;
 	public int daysActive;
 	AccountType accountType;
-	
-	public Account(double value, double monthlyRate, AccountType accountType) throws Exception {
-		if (value < 0) {
-			loanValue = value;
+
+	public Account(double loanValue, double monthlyRate, AccountType accountType) throws Exception {
+		if (loanValue < 0) {
+			throw new Exception();
 		}
+		if (monthlyRate < 0) {
+			throw new Exception();
+		}
+		this.loanValue = loanValue;
 		this.monthlyRate = monthlyRate;
 		this.accountType = accountType;
-	}	
+	}
 
 	public double getLoanValue() {
 		System.out.println("The loan value is " + this.loanValue);
@@ -49,26 +57,34 @@ public class Account {
 		stringBuilder.append("Account type: ");
 		stringBuilder.append(this.accountType);
 		stringBuilder.append(System.getProperty("line.separator"));
-		return stringBuilder.toString();}
+		return stringBuilder.toString();
+	}
 
-	public void print() {
-		int vb = 10;
-		System.out.println("This is an account");
+	public static double getDifferenceInterestAndPrincipal(Account account) {
+		return (account.daysActive / DAYS_IN_A_YEAR) - account.loanValue;
+	}
+
+	public static double getAccountInterestFee(Account account) {
+		return Math.pow(account.monthlyRate, getDifferenceInterestAndPrincipal(account));
+	}
+
+	public static double getLoanValueAfterInterestFee(Account account) {
+		return account.loanValue * getAccountInterestFee(account);
+	}
+
+	public static double getAccountFinalFee(Account account) {
+		return BROKER_FEE * getLoanValueAfterInterestFee(account);
 	}
 
 	public static double getPremiumAccountsTotalFees(Account[] accounts) {
 		double totalFee = 0.0;
-		Account account;
-		int temp = 365;
 		for (int i = 0; i < accounts.length; i++) {
-			account = accounts[i];
-			if (account.accountType == AccountType.PREMIUM || account.accountType == AccountType.SUPER_PREMIUM)
-				totalFee += .0125 * ( 
-				account.loanValue * Math.pow(account.monthlyRate, (account.daysActive / 365)) - account.loanValue); 
+			if (accounts[i].accountType == AccountType.PREMIUM
+					|| accounts[i].accountType == AccountType.SUPER_PREMIUM) {
+				totalFee += getAccountFinalFee(accounts[i]);
+			}
 		}
 		return totalFee;
 	}
-
-
 
 }
---
+++ Assignment 2/src/exceptions/InvalidInputException2.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class InvalidInputException2 extends Exception {
+
+	public InvalidInputException2(String string) {
+		
+	}
+
+}
